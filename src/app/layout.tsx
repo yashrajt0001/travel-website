5ed0a3f4556e06f553cf6c166a03947be9af391c
yashrajt0001 <yeshrajteli12345@gmail.com>
2025-06-05 19:13:52 +0530

    added ai bot

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Playfair_Display } from "next/font/google";
+import Script from "next/script"; // ⬅️ Import Script from next/script
 import "./globals.css";
 
 const geistSans = Geist({
@@ -35,6 +36,22 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} ${playfairDisplay.variable} antialiased`}
       >
         {children}
+
+        {/* ✅ Tawk.to Live Chat Script */}
+        <Script id="tawkto" strategy="afterInteractive">
+          {`
+            var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+            (function(){
+              var s1 = document.createElement("script"),
+                  s0 = document.getElementsByTagName("script")[0];
+              s1.async = true;
+              s1.src = 'https://embed.tawk.to/68419232f046fc190b82eaf6/1it02664l';
+              s1.charset = 'UTF-8';
+              s1.setAttribute('crossorigin', '*');
+              s0.parentNode.insertBefore(s1, s0);
+            })();
+          `}
+        </Script>
       </body>
     </html>
   );
